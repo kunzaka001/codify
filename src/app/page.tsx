7eca5aaddcee34abb308aa3619b9b34dec322be1
kunzaka001 <137798282+kunzaka001@users.noreply.github.com>
2024-11-24 17:04:56 +0700

    Create And Update Codify

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,140 @@
+"use client";
+
 import Image from "next/image";
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+import { Button } from "@/components/ui/button";
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+import app from "../../firebase-config";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
+import {
+  BarChartHorizontal,
+  BatteryCharging,
+  CircleHelp,
+  Layers,
+  WandSparkles,
+  ZoomIn,
+} from "lucide-react";
+
+import CodifyLogo from "./assets/CodifyNewLogo.png";
+
+export default function Home() {
+
+  const signinWithGoogle = async () => {
+    const auth = getAuth(app);
+    const provider = new GoogleAuthProvider();
+    try {
+      const result = await signInWithPopup(auth, provider);
+      console.log(result)
+      // ...
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (error: any) {
+      console.error("Error signing in with Google:", error.message);
+    }
+  };
+
+  const reasons = [
+    {
+      title: "Topic-Based Quizzes",
+      description:
+        "Choose from a range of development topics like web development, data science, and cybersecurity, tailored for developers at all skill levels to sharpen their expertise.",
+      icon: <ZoomIn className="size-6" />,
+    },
+    {
+      title: "Global Leaderboards",
+      description:
+        "Compete with developers worldwide and see where you stand. Track your progress, compare scores, and strive for the top in your favorite topics.",
+      icon: <BarChartHorizontal className="size-6" />,
+    },
+    {
+      title: "Difficulty Adjustment",
+      description:
+        "Increase or decrease in difficulty based on your knowledge, And improved more..",
+      icon: <CircleHelp className="size-6" />,
+    },
+    {
+      title: "Timed Quiz",
+      description:
+        "Challenge yourself with fast-paced, tech-focused questions that push your knowledge and speed. Perfect for testing your expertise in a high-stakes environment!?",
+      icon: <WandSparkles className="size-6" />,
+    },
+    {
+      title: "NULL",
+      description: "NULL",
+      icon: <Layers className="size-6" />,
+    },
+    {
+      title: "NULL",
+      description: "NULL",
+      icon: <BatteryCharging className="size-6" />,
+    },
+  ];
+
+  return (
+    <>
+      <section className="flex justify-center items-center pt-32">
+        <div className="container flex justify-center items-center">
+          <div className="grid items-center gap-8 lg:grid-cols-2 justify-center">
+            <div className="flex flex-col items-center text-center lg:items-start lg:text-left">
+              <h1 className="my-6 text-pretty text-4xl font-bold lg:text-6xl">
+                <span className="text-green-500">Codify</span> for Developers:
+                Test and Elevate Your Tech Skills
+              </h1>
+              <p className="mb-8 max-w-xl text-muted-foreground lg:text-xl">
+                Boost your tech skills with Codify for Devs—an interactive quiz
+                platform designed for developers. Tackle questions on coding,
+                tech trivia, and best practices to learn, practice, and stay
+                sharp.
+              </p>
+              <div className="flex w-full flex-col justify-center gap-2 sm:flex-row lg:justify-start">
+                <Button
+                  className="w-full jello-vertical sm:w-auto"
+                  onClick={signinWithGoogle}
+                >
+                  Dive In!
+                </Button>
+                <Button
+                  variant="outline"
+                  className="w-full jello-vertical sm:w-auto"
+                >
+                  Github
+                  {/*<ArrowDownRight className="ml-2 size-4" />*/}
+                </Button>
+              </div>
+            </div>
+            <div className="rotate-in-center">
+              <Image
+                src={CodifyLogo}
+                alt="Codify Logo"
+                className="max-h-96 w-full rounded-md object-cover"
+              />
+            </div>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </section>
+
+      <hr className="w-48 h-1 mx-auto my-4 bg-gray-100 border-0 rounded md:my-10 dark:bg-gray-700" />
+
+      <section className="flex justify-center items-center">
+        <div className="container">
+          <div className="mb-10 md:mb-20">
+            <h2 className="mb-2 text-center text-3xl font-semibold lg:text-5xl">
+              Features
+            </h2>
+          </div>
+          <div className="grid gap-10 md:grid-cols-2 lg:grid-cols-3">
+            {reasons.map((reason, i) => (
+              <div key={i} className="flex flex-col">
+                <div className="mb-5 flex size-16 items-center justify-center rounded-full bg-accent">
+                  {reason.icon}
+                </div>
+                <h3 className="mb-2 text-xl font-semibold">{reason.title}</h3>
+                <p className="text-muted-foreground">{reason.description}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
