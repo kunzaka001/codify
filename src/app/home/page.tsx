c80d6b5a4d034dd835b8ca4584387f687524e947
kunzaka001 <137798282+kunzaka001@users.noreply.github.com>
2024-12-12 22:43:31 +0700

    Initialized home page

---
+++ src/app/home/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+export default function Home() {
+    
+    
+    return(
+        <>
+            <title>Homepage</title>
+            <div className="h-screen flex justify-center items-center">
+                <h1 className="font-bold">THis is a home page</h1>
+            </div>
+        </>
+    )
+}
