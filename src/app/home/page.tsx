3ab0e75f6d97b3285e49947be7aed056fdae97df
kunzaka001 <kunnoppakun007@gmail.com>
2025-01-09 22:21:48 +0700

    Update page.tsx

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,13 +1,15 @@
 "use client";
+import { useEffect } from "react";
+
 export default function Home() {
-    
-    
-    return(
-        <>
-            <title>Homepage</title>
-            <div className="h-screen flex justify-center items-center">
-                <h1 className="font-bold">THis is a home page</h1>
-            </div>
-        </>
-    )
+  useEffect(() => {}, []);
+
+  return (
+    <>
+      <title>Homepage</title>
+      <div className="h-screen flex justify-center items-center">
+        <h1 className="font-bold">THis is a home page</h1>
+      </div>
+    </>
+  );
 }
